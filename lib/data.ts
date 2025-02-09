66875e2a2972ae9464a8b706ff3042e0e496eb91
Sagar <sagargound0410@gmail.com>
2025-02-09 12:26:57 +0530

    besocial project added

--- lib/data.ts
+++ lib/data.ts
@@ -4,7 +4,7 @@ import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import africasiaImg from "@/public/africasia.png";
 import unity from "@/public/unity.png";
-// import wordanalyticsImg from "@/public/wordanalytics.png";
+import besocial from "@/public/besocial.png";
 
 export const links = [
   {
@@ -46,7 +46,7 @@ export const experiencesData = [
     title: "Front-End Developer",
     location: "Vashi, Navi Mumbai, India",
     description:
-      "I working as a front-end developer for a year now. I also learned new technologys like React and Next.js.",
+      "I working as a front-end developer for 2 years now. I also learned new technologys like React.js and Next.js.",
     icon: React.createElement(FaReact),
     date: "2023 - Present",
   },
@@ -67,27 +67,28 @@ export const projectsData = [
     tags: ["React", "JavaScript", "Next.js", "React Bootstrap",],
     imageUrl: unity,
   },
-  // {
-  //   title: "Word Analytics",
-  //   description:
-  //     "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-  //   tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-  //   imageUrl: wordanalyticsImg,
-  // },
+  {
+    title: "BeSocial",
+    description:
+      "I built a MERN stack project with a social media-like UI, enabling users to perform CRUD operations on users and posts, follow/unfollow users, and connect frontend with backend via a server.",
+    tags: ["React.js", "MongoDB", "Node.js", "Express.js"],
+    imageUrl: besocial,
+  },
 ] as const;
 
 export const skillsData = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "TypeScript",
   "React",
   "Next.js",
   "Node.js",
-  "Git",
-  "Bootstrap",
   "MongoDB",
+  "JavaScript",
   "Redux",
   "Express",
+  "HTML",
+  "CSS",
+  "Bootstrap 5",
   "Framer Motion",
+  "TypeScript",
+  "Git",
+  "GitHub",
 ] as const;
